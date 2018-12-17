3a3af0cd78ded92c02e24a4037ec1a41e612554d
Tyler Duddy <tmd44@pitt.edu>
2018-12-17 11:34:42 -0500

    Change data sharing to  uncheck all boxes

--- data-sharing.py
+++ data-sharing.py
@@ -29,18 +29,16 @@ def block_data_sharing(id_params):
             break
         checkboxes = css_select('[role="checkbox"]', multiple=True, driver=driver)
         sleep(0.1)
-        
-    # ensure that boxes 0, 1, 3, 4 are unchecked and box 2 is checked
-    for i in range(len(checkboxes)):
-        check_box = checkboxes[i]
-        check_class = check_box.get_attribute('class')
-        if 'checked' in check_class and i != 2:
-            check_box.click()
-        elif 'checked' not in check_class and i==2:
-            check_box.click()
+
+    # uncheck all data_sharing boxes.
+    for box in checkboxes:
+        box_class = box.get_attribute('class')
+        if 'checked' in box_class:
+            box.click()
         else:
             continue
-        
+    
+    # save and print confirmation
     save_button = css_select('[type="submit"]', driver=driver)
     save_button.click()
     print("Finished blocking data sharing, moving on")
@@ -49,13 +47,16 @@ def block_data_sharing(id_params):
 def main():
     driver.get('https://analytics.google.com')
     sleep(0.2)
+
+    # check for sign-in page
     current_url = driver.current_url
     if("accounts.google.com/signin/v2/identifier" in current_url):
         sign_in(driver=driver)
 
+    # cycle through provided URLs and execute data_sharing function
     for id_params in URLS:
         block_data_sharing(id_params)
-
+    
     check_continue('Done, enter "y" to exit: ', driver=driver)     
     driver.quit()
 
